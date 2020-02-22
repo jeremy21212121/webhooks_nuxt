3859a7bb491503fdbd7ae0ec9d80962ed2baa99e
Jeremy <me@jeremypoole.ca>
2020-02-21 21:43:33 -0800

    change route paths to troubleshoot webhook post from github getting 404'd

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 const port = 7777
 const host = '127.0.0.1'
-const frontEndRoute = '/trivia/frontend'
-const backEndRoute = '/trivia/backend'
+const frontEndRoute = '/frontend'
+const backEndRoute = '/backend'
 const secrets = require(path.resolve('./secrets/trivia.js'))
 
 module.exports = { port, host, frontEndRoute, backEndRoute, secrets }
