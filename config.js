95bdce4ed9339968c3bbe6716c41227212ae8f75
Jeremy <me@jeremypoole.ca>
2020-02-21 18:41:51 -0800

    adjust route paths. it is slightly confusing as it is reverse-proxied by nginx, so only the part of the path after '/webhooks/v1' is passed to the application.

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 const port = 7777
 const host = '127.0.0.1'
-const baseRoute = '/webhooks'
-const frontEndRoute = '/webhooks/trivia/frontend'
-const backEndRoute = '/webhooks/trivia/backend'
+const baseRoute = '/trivia'
+const frontEndRoute = '/trivia/frontend'
+const backEndRoute = '/trivia/backend'
 const secrets = require(path.resolve('./secrets/trivia.js'))
 
 module.exports = { port, host, baseRoute, frontEndRoute, backEndRoute, secrets }
