7123deb3f3d56a8bb8bda805aed7dc9e08e08d33
Jeremy <me@jeremypoole.ca>
2020-02-21 20:49:55 -0800

    fix ReferenceError created by last commit

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ const path = require('path')
 
 const port = 7777
 const host = '127.0.0.1'
-//const baseRoute = '/trivia'
 const frontEndRoute = '/trivia/frontend'
 const backEndRoute = '/trivia/backend'
 const secrets = require(path.resolve('./secrets/trivia.js'))
 
-module.exports = { port, host, baseRoute, frontEndRoute, backEndRoute, secrets }
+module.exports = { port, host, frontEndRoute, backEndRoute, secrets }
